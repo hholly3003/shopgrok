569a0ec16d9636bd7e1cd1df6a6d0529ccdbedcc
Holly <h.holly3003@gmail.com>
2021-03-29 21:04:08 +1100

    update the format of the output file

--- aldi/aldi/spiders/aldispiders.py
+++ aldi/aldi/spiders/aldispiders.py
@@ -21,10 +21,11 @@ class AldiSpider(scrapy.Spider):
         all_products = response.css("div.m-text-image")
 
         for product in all_products:
-            product_title = product.css("div.box--description--header::text").extract()
+            product_title = product.css("div.box--description--header::text").extract()[0].replace("\xa0"," ").strip()
             product_image = product.css('.m-no-ratio-on-phone img').css('::attr(src)').extract()
             packsize = product.css("span.box--amount::text").extract()
             price = product.css(".box--decimal , .ym-hideme+ .box--value").css('::text').extract()
+            price = price[0] + price[1]
             price_per_unit = product.css("span.box--baseprice::text").extract()
 
             items["product_title"] = product_title
@@ -33,4 +34,18 @@ class AldiSpider(scrapy.Spider):
             items["price"] = price
             items["price_per_unit"] = price_per_unit
         
-            yield items
+            yield {
+                "product_title": product_title,
+                "product_image": product_image,
+                "packsize":packsize,
+                "price":price,
+                "price_per_unit" : price_per_unit
+            }
+        
+        if not all_products:
+            sub_category = response.css('.csc-textepic-imagecolumn a').css('::attr(href)').extract_first()
+            print(sub_category)
+            print("***************")
+            # if sub_category:
+                # yield scrapy.Request(response.urljoin(sub_category), callback=self.parse)
+        
